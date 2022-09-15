7d3c24474532b14e5408c9e475f5e9654b4adea0
Andrew Yfantis <andrew_yfantis@yahoo.com>
2022-09-14 21:47:22 -0700

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+docker build -t ayfantis53/multi-client-k8s:latest -t ayfantis53/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t ayfantis53/multi-server-k8s-pgfix:latest -t ayfantis53/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t ayfantis53/multi-worker-k8s:latest -t ayfantis53/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push ayfantis53/multi-client-k8s:latest
+docker push ayfantis53/multi-server-k8s-pgfix:latest
+docker push ayfantis53/multi-worker-k8s:latest
+
+docker push ayfantis53/multi-client-k8s:$SHA
+docker push ayfantis53/multi-server-k8s-pgfix:$SHA
+docker push ayfantis53/multi-worker-k8s:$SHA
+
+echo "starting"
+aws eks --region ap-south-1 describe-cluster --name test001 --query cluster.status
+
+echo "applying k8 files"
+kubectl apply -f ./k8s/
+
+kubectl set image deployments/server-deployment server=ayfantis53/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=ayfantis53/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=ayfantis53/multi-worker-k8s:$SHA
+
+echo "done"
